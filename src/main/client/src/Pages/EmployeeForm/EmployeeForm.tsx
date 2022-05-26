24d0413aef520941b2dc2430ff443183ab4a3fc4
gcsabika <Asd198523654>
2022-05-26 13:04:36 +0300

    Intro for the basics of frontend, css

--- src/main/client/src/Pages/EmployeeForm/EmployeeForm.tsx
+++ src/main/client/src/Pages/EmployeeForm/EmployeeForm.tsx
@@ -1,6 +1,7 @@
 import {Button, TextField} from "@mui/material";
 import {useState} from "react";
 import {blankEmployee, Employee, EmployeeError} from "./EmployeeForm.types";
+import './EmployeeForm.css';
 
 export type EmployeeFormProps = {
     employee: Employee,
@@ -12,10 +13,11 @@ export type EmployeeFormProps = {
 export function EmployeeForm(props: EmployeeFormProps) {
 
     return (
-        <form autoCapitalize={'off'}>
+        <form autoCapitalize={'off'} className={"employee-form__form"}>
+            <h1>Employee</h1>
             <TextField
                 value={props.employee.firstName}
-                placeholder={'First Name'}
+                label={'First Name'}
                 onChange={(e) => {
                     const newEmployee: Employee = {...props.employee, firstName: e.currentTarget.value}
                     props.setEmployee(newEmployee)
@@ -26,13 +28,13 @@ export function EmployeeForm(props: EmployeeFormProps) {
 
             <TextField
                 value={props.employee.lastName}
-                placeholder={'Last Name'}
+                label={'Last Name'}
                 onChange={(e) => {
                     const newEmployee: Employee = {...props.employee, lastName: e.currentTarget.value}
                     props.setEmployee(newEmployee)
             }}/>
 
-            <Button onClick={(e)=> {
+            <Button variant={"contained"} onClick={(e)=> {
                 console.log(props.submit)
             }}>
                 Submit
